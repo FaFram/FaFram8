43f1c2b4fe021a04ed8445e6493c74ac3bc12f15
Tomas Plevko <tplevko@redhat.com>
2016-10-18 15:26:00 +0200

    [relates #190] Update jclouds version

--- fafram8-ec2/src/test/java/org/jboss/fuse/qa/fafram8/ec2/provision/StartStopTest.java
+++ fafram8-ec2/src/test/java/org/jboss/fuse/qa/fafram8/ec2/provision/StartStopTest.java
@@ -27,7 +27,7 @@ public class StartStopTest {
 	}
 
 	@Test
-	public void testOverridingValues() throws Exception {
+	public void testStartStopOperations() throws Exception {
 
 		ec2Client = Ec2Client.builder().defaultEc2client().build();
 
@@ -35,15 +35,15 @@ public class StartStopTest {
 		final NodeMetadata server = ec2Client.getServerFromRegister(ec2Client.getNamePrefix() + "-" + nodeName);
 		ec2Client.stopServer(server.getName());
 
-		log.info("Wait 20 seconds for the server to stop");
-		Thread.sleep(20000);
+		log.info("Wait 30 seconds for the server to stop");
+		Thread.sleep(30000);
 
 		Assert.assertEquals("SUSPENDED", ec2Client.getServers(server.getName()).get(0).getStatus().name());
 
 		ec2Client.startServer(server.getName());
 
-		log.info("Wait 20 seconds for the server to start");
-		Thread.sleep(20000);
+		log.info("Wait 30 seconds for the server to start");
+		Thread.sleep(30000);
 
 		Assert.assertEquals("RUNNING", ec2Client.getServers(server.getName()).get(0).getStatus().name());
 	}
